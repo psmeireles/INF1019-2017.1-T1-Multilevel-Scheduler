9b51860d59f94838f7e9578a984f2779d3ef55a5
psmeireles <psmeireles25@gmail.com>
2017-10-19 19:53:32 -0200

    ajuste da struct processo

--- processo.h
+++ processo.h
@@ -1,10 +1,17 @@
+
+typedef enum estado{
+	novo,
+	pronto,
+	esperando,
+	executando,
+	terminado
+} Estado;
+
 typedef struct Processo{
 
-	int num_rajadas;
 	int rajadas[50];
-	int tempo_restante;
-	int rajadas_restantes;
-	int fila_atual;
-	char* nome;
+	int fila;
+	int prox_fila;
+	Estado estado;
 	int pid;
 } Processo;
