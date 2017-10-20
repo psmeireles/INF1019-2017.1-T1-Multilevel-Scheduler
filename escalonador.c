b89ce34acd187e03892ed9f4d95c908b33073867
psmeireles <psmeireles25@gmail.com>
2017-10-20 20:21:23 -0200

    adicionado tratamento para múltiplos processos em I/O

--- escalonador.c
+++ escalonador.c
@@ -13,11 +13,11 @@
 
 Processo *pcorrente, *pIO;
 pid_t pid_escalonador;
-Fila *level1, *level2, *level3;
+Fila *level1, *level2, *level3, *io;
 
 int flag_IO = 0, flag_Child = 0;
 
-int RoundRobin(Fila *f);
+void RoundRobin(Fila *f);
 
 int ExecutarProcesso(Processo *p, int t);
 
@@ -39,6 +39,7 @@ int main(){
     level1 = fila_cria(1);
     level2 = fila_cria(2);
     level3 = fila_cria(4);
+    io = fila_cria(3);
     
     signal(SIGALRM, alarmHandler);
     signal(SIGUSR2, IOHandler);		// Começa a escutar sinal de que entrou em I/O
@@ -76,8 +77,20 @@ int main(){
 }
 
 
-int RoundRobin(Fila *f){    // Executa o escalonamento Round Robin da fila recebida
-	int ret;
+void RoundRobin(Fila *f){    // Executa o escalonamento Round Robin da fila recebida
+    int ret;
+    
+    // Verifica se algum processo foi inserido em uma fila superior
+    /*if(fila_tempo(f) == 2){
+        if(!fila_vazia(level1)){
+            return;
+        }
+    }
+    else if(fila_tempo(f) == 4){
+        if(!fila_vazia(level1) || !fila_vazia(level1)){
+            return;
+        }
+    }*/
 	
     while(!fila_vazia(f)){
         pcorrente = fila_retira(f);
@@ -97,11 +110,23 @@ int RoundRobin(Fila *f){    // Executa o escalonamento Round Robin da fila receb
 			/* TODO: Preciso saber se tenho que retornar algo avisando que tem que voltar pra fila de cima ou se continuo executando */
         }
         else{   // Processo terminou
-			//free(pcorrente); -> Aqui deu um erro monstro
+			//free(pcorrente);// -> Aqui deu um erro monstro
 			// Só isso?
         }
+
+        // Verifica se algum processo foi inserido em uma fila superior
+        /*if(fila_tempo(f) == 2){
+            if(!fila_vazia(level1)){
+                return;
+            }
+        }
+        else if(fila_tempo(f) == 4){
+            if(!fila_vazia(level1) || !fila_vazia(level1)){
+                return;
+            }
+        }*/
     }
-    return 0;
+    return;
 }
 
 
@@ -113,9 +138,11 @@ int ExecutarProcesso(Processo *p, int t){    // Executa um novo processo ou cont
     pid_t pid_novo;
     sigset_t mask, oldmask;
 
+    // Determinando quais sinais serão ouvidos durante o sleep
     sigemptyset(&mask);
     sigaddset(&mask, SIGUSR2);
     sigaddset(&mask, SIGCHLD);
+    sigaddset(&mask, SIGALRM);
 
 	printf("L%d\n", p->fila);
 
@@ -126,14 +153,11 @@ int ExecutarProcesso(Processo *p, int t){    // Executa um novo processo ou cont
         kill(p->pid, SIGSTOP);
         sigprocmask (SIG_UNBLOCK, &mask, NULL);
         if(flag_IO == 1){ // Entrou em I/O
-            printf("Flag_IO = 1\n");
-            alarm(3);
             flag_IO = 0;
             p->estado = esperando;
             return 1;
         }
         if(flag_Child == 1){	// Terminou de executar
-            printf("Flag_Child = 1\n");
             flag_Child = 0;
             p->estado = terminado;
             return 2;
@@ -157,14 +181,11 @@ int ExecutarProcesso(Processo *p, int t){    // Executa um novo processo ou cont
             kill(p->pid, SIGSTOP);
             sigprocmask (SIG_UNBLOCK, &mask, NULL);
             if(flag_IO == 1){	// Entrou em I/O
-                printf("Flag_IO = 1\n");
-                alarm(3);
                 flag_IO = 0;
                 p->estado = esperando;
                 return 1;
             }
             if(flag_Child == 1){	// Terminou de executar
-                printf("Flag_Child = 1\n");
                 flag_Child = 0;
                 p->estado = terminado;
                 return 2;
@@ -199,38 +220,47 @@ char** Preenche_argv(Processo *p){  // Preenche argv com os parâmetros relativo
 }
 
 void IOHandler(int sinal){
-	kill(pcorrente->pid, SIGSTOP);
-    printf("Filho entrou em I/O\n");
-	pcorrente->prox_fila = pcorrente->fila - 1;
-	if(pcorrente->prox_fila == 0)
-		pcorrente->prox_fila = 1;
-	pIO = pcorrente;
-    flag_IO = 1;
-    // TODO: Descobrir como fazer com que os alarmes ocorram em paralelo
+    kill(pcorrente->pid, SIGSTOP);
+    if(fork() == 0){ // Filho
+        sleep(3);
+        kill(getppid(), SIGALRM);
+        kill(getpid(), SIGSTOP);
+    }
+    else{   // Pai
+        printf("%d entrou em I/O\n", pcorrente->pid);
+        pcorrente->prox_fila = pcorrente->fila - 1;
+        if(pcorrente->prox_fila == 0)
+            pcorrente->prox_fila = 1;
+        fila_insere(io, pcorrente);
+        flag_IO = 1;
+    }
 }
 
 void ChildHandler(int sinal){
-	int pid_encerrado = waitpid(-1, NULL, WNOHANG);
+    int pid_encerrado = waitpid(-1, NULL, WNOHANG);
 	if(pid_encerrado == 0){ // SIGSTOP ou SIGCONT
 		return;
 	}
-    printf("Filho mandou SIGCHLD\n");
+    printf("%d mandou SIGCHLD\n", pid_encerrado);
     flag_Child = 1;
-    // free(pcorrente); -> acho que aqui é o melhor lugar pro free
+    //-> acho que aqui é o melhor lugar pro free
 }
 
-// Promissor, mas ainda precisa de mais testes
 void alarmHandler(int sinal){
     signal(SIGALRM, SIG_IGN);
+    printf("Recebeu alarme\n");
+    pIO = fila_retira(io);
     pIO->estado = pronto;
+    printf("%d voltou pra fila ", pIO->pid);
 	if(pIO->prox_fila == 2){
-		pIO->fila = 2;
+        pIO->fila = 2;
+        printf("2\n");
 		fila_insere(level2, pIO);
 	}
 	else{
-		pIO->fila = 1;
-		fila_insere(level1, pIO);
+        pIO->fila = 1;
+        printf("1\n");
+        fila_insere(level1, pIO);
     }
     signal(SIGALRM, alarmHandler);
-	// Como fazer pro alarme saber em que fila ele tem que botar? Acho que resolvi
 }
